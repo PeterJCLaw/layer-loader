f40cfd6e6724deeb1e7c9d3df08ca330822eae7d
Peter Law <PeterJCLaw@gmail.com>
2020-02-01 20:59:53 +0000

    There can't be bytes here

--- layer_loader/types.py
+++ layer_loader/types.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List, Union
 
-LayerElement = Union[bool, bytes, float, int, str, None, List[Any], Dict[str, Any]]
+LayerElement = Union[bool, float, int, str, None, List[Any], Dict[str, Any]]
 Layer = Dict[str, LayerElement]
 Path = List[str]
