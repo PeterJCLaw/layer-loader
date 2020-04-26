8b662a48e0b1af4e9a236996c11c20ffeeae8943
Peter Law <PeterJCLaw@gmail.com>
2020-04-26 18:02:48 +0100

    Also type check setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup  # type: ignore[import]
 
 with open('README.md') as file:
     long_description = file.read()
